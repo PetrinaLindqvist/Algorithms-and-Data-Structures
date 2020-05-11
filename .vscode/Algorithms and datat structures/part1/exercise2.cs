4306c8c85fa457cf13265e90a20bca2b4313aeec
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-05-11 12:37:26 +0300

    exercise 2 done

--- .vscode/Algorithms and datat structures/part1/exercise2.cs
+++ .vscode/Algorithms and datat structures/part1/exercise2.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Text.RegularExpressions;
-
 namespace part1
 {
   public class Substrings
@@ -10,29 +8,32 @@ namespace part1
         int first = a.Length;         
         int second = b.Length;         
         int res = 0; 
-   
+        
         /* A loop to slide pat[] one by one */
-        for (int i = 0; i <= second - first; i++) 
+        for (int i = 0; i <= first - second; i++) 
         { 
             /* For current index i, check for  
         pattern match */
-            int j;             
-            for (j = 0; j < first; j++) 
+            int j;       
+            for (j = 0; j < second; j++) 
             { 
-                if (b[i + j] != a[j]) 
+                 
+                if (a[i + j] != b[j]) 
                 { 
                     break; 
                 } 
             } 
    
             // if pat[0...M-1] = txt[i, i+1, ...i+M-1]  
-            if (j == first) 
+            if (j == second) 
             {                 
                 res++;                 
                 j = 0;                 
             }             
+        
+         
         }         
-        return res;       
+        return res;  
       
       
     }
