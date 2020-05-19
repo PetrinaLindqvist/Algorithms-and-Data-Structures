74963477e6e711865d51a06ce561dd658b2e2117
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-05-19 19:45:43 +0300

    exercise 5 something

--- .vscode/Algorithms and datat structures/part2/exercise5.cs
+++ .vscode/Algorithms and datat structures/part2/exercise5.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 
 namespace part2
 {
@@ -6,9 +6,25 @@ namespace part2
     {
         public int Calculate(int[] t)
         {
+          int leftSum = 0;
+        
+          for (int i = 0; i < t.Length; i++)
+          {
+              leftSum += t[i];
+                 
+               int rightSum = 0;    
+               for (int j = i + 1; j < t.Length; j++) 
+               
+                 rightSum += t[j];
 
+              if(leftSum == rightSum)
+              return i +1;  
+          }
+          
+          return -1;
+        
         }
 
     }
 
-*/
+}
