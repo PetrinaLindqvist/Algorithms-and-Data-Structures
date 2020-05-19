a3107914c5b4192dcdf0a01b7e0c024515e7565f
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-05-19 14:09:47 +0300

    exercise 4 done

--- .vscode/Algorithms and datat structures/part2/Program.cs
+++ .vscode/Algorithms and datat structures/part2/Program.cs
@@ -25,17 +25,18 @@ namespace part2
             Console.WriteLine(p.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 5*/
 
             //Ex.3
-            Changes m = new Changes();
+            /*Changes m = new Changes();
             Console.WriteLine(m.Calculate(new int[] { 1, 1, 2, 2, 2 })); // 2
             Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
-            Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2
+            Console.WriteLine(m.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 2*/
 
 
             //Ex.4
-            /*Revolutions r = new Revolutions();
-            Console.WriteLine(m.Calculate(new int[] {4,1,3,2,5})); // 3
-            Console.WriteLine(m.Calculate(new int[] {1,2,3,4,5})); // 1
-            Console.WriteLine(m.Calculate(new int[] {5,4,3,2,1})); // 5*/
+            Revolutions r = new Revolutions();
+            Console.WriteLine(r.Calculate(new int[] {4,1,3,2,5})); // 3
+            Console.WriteLine(r.Calculate(new int[] {1,2,3,4,5})); // 1
+            Console.WriteLine(r.Calculate(new int[] {5,4,3,2,1})); // 5
+            Console.WriteLine(r.Calculate(new int[] {5,4,2,1,3})); // 4
 
             //Ex.5
             /*Split s = new Split();
--- .vscode/Algorithms and datat structures/part2/exercise4.cs
+++ .vscode/Algorithms and datat structures/part2/exercise4.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 
 namespace part2
 {
@@ -6,9 +6,27 @@ namespace part2
     {
         public int Calculate(int[] t)
         {
-
+            int revolutions = 0;
+            int counter = 1;
+            for (int i = 1; i <= t.Length; i++)
+            {
+                for (int j = 0; j <= t.Length - 1; j++)
+                {
+                    if (t[j] == counter)
+                    {
+                        counter ++;
+                    
+                    if(counter == t.Length +1)
+                    {
+                        revolutions = i;
+                    }
+                    }
+                }
+            }
+            return revolutions;
+        
         }
 
     }
 
-}*/
+}
