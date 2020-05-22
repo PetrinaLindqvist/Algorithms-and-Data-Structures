fa14cd35b9c19d67a3390e65f9cfeca24dfaf1cb
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-05-22 10:54:19 +0300

    exercise 2 done

--- .vscode/Algorithms and datat structures/part3/Program.cs
+++ .vscode/Algorithms and datat structures/part3/Program.cs
@@ -17,6 +17,19 @@ namespace part3
             Console.WriteLine(s.Calculate(new int[] { 1, 10, 100 })); // 9
             Console.WriteLine(s.Calculate(new int[] { 1, 1, 1, 1, 1 })); // 0
             Console.WriteLine(s.Calculate(Randomizer(10))); // depends on random
+        }
+        public static int[] Randomizer(int n)
+        {
+                Random random = new Random();
+                int[] arr = new int[n];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    // integers between 1 and 1000 are enough for us
+                    arr[i] = random.Next(1, 1001);
+                }
+                return arr;
+            }
+
 
             //Ex.3
             /*Sorting s = new Sorting();
@@ -26,7 +39,7 @@ namespace part3
             s.MergeSort(sortMe);
             s.QuickSort(sortMeLarge);
             s.MergeSort(sortMeLarge);*/
-            
+
             //Ex.4
             /*BinarySearch b = new BinarySearch();
             Console.WriteLine(b.Find((new int[] {4,1,8,5}, 2)); // false
@@ -34,7 +47,7 @@ namespace part3
             Console.WriteLine(b.Find((new int[] {4,1,8,5,8,7,4,,2,3}, 2)); // true
             Console.WriteLine(b.Find((new int[] {0}, 0)); // true
             onsole.WriteLine(s.Calculate(Randomizer(100000), 3)); // ?*/
-            
+
             //Ex.5
             /*Inversions inv = new Inversions();
             int[] t = inv.Create(5, 2);
@@ -45,4 +58,4 @@ namespace part3
 
         }
     }
-}
+
--- .vscode/Algorithms and datat structures/part3/exercise2.cs
+++ .vscode/Algorithms and datat structures/part3/exercise2.cs
@@ -7,23 +7,24 @@ namespace part3
     public class SmallestDifference
     {
 
-    public int Calculate(int[] t)
-    {
-        
-
-    }
-
-    public static int[] Randomizer(int n)
-    {
-        Random random = new Random();
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.Length; i++)
+        public int Calculate(int[] t)
         {
-            // integers between 1 and 1000 are enough for us
-            arr[i] = random.Next(1, 1001);
+            int diff = int.MaxValue;
+            for (int i = 0; i < t.Length; i++)
+            {
+                for (int j = i + 1; j <t.Length; j++ )
+                {
+                    if (Math.Abs((t[i] - t[j])) < diff)
+                    {
+                     diff = Math.Abs((t[i] - t[j]));
+                    }
+                }
+            }
+            return diff;
+
         }
-        return arr;
-    }
+
+   
 
     }
 }
