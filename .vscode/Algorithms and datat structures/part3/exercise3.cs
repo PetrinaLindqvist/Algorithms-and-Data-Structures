6105a30a5eca0872670e51ebbed97ceb7169d33c
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-05-22 12:32:59 +0300

    exercise 4 done

--- .vscode/Algorithms and datat structures/part3/exercise3.cs
+++ .vscode/Algorithms and datat structures/part3/exercise3.cs
@@ -9,7 +9,30 @@ namespace part3
         public void MergeSort(int[] t)
         {
         DateTime start = DateTime.Now;
+
+        int a = 0;
+        int b = t.Length - 1;
+        int k = (a + b)/2;
+        int counter = 0;
+        }
+        public void Sort(a, b)
+        {
+        if (a == b)
+        {
+            return;
+        }
         
+        sort(a,k);
+        sort(k +1, b)
+        ____________
+        sort(a,b)
+        if a == b
+            return
+        k = (a+b)/2
+        sort(a,k)
+        sort(k+1,b)
+        merge(a,k,k+1,b)
+        ________________
         DateTime end = DateTime.Now;
         Console.WriteLine("Time this took: " + end.Subtract(start));
         }
@@ -18,6 +41,20 @@ namespace part3
         public void QuickSort(int[] t)
         {
         DateTime start1 = DateTime.Now;
+
+        merge(a1, b1, a2, b2)
+        merge(a1, b1, a2, b2)
+        a = a1, b = b2
+        for i = a to b
+            if a2 > b2 or (a1 <= b1 and array[a1] <= array[a2])
+            help[i] = array[a1]
+            a1 += 1
+            else
+            help[i] = array[a2]
+            a2 += 1
+        for i = a to b
+            array[i] = help[i]
+
         DateTime end1 = DateTime.Now;
         Console.WriteLine("Time this took: " + end1.Subtract(start1));
         }  
