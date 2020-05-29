251fca37e9fcf2ba3c37f35d4d0ff05fb850661c
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-05-29 14:12:54 +0300

    done

---
+++ .vscode/Algorithms and datat structures/part4/exercise5/BinarySearchTree.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+namespace part4
+{
+    public class BinarySearchTree 
+    {
+    
+        public void Add(int x)
+        {
+
+        }
+        
+        public int Height()
+        {
+            
+        }
+    
+    
+    }
+
+}
+
+    
