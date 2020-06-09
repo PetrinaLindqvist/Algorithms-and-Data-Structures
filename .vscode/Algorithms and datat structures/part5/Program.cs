76e522a37123d8c6ec378180fb91a233710e736b
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-06-09 19:44:57 +0300

    exercise 4 done

--- .vscode/Algorithms and datat structures/part5/Program.cs
+++ .vscode/Algorithms and datat structures/part5/Program.cs
@@ -16,16 +16,16 @@ namespace part5
             Console.WriteLine(c.Calculate(1)); // 3*/
 
             //Ex3
-            Components k = new Components(6);
+            /*Components k = new Components(6);
             k.AddConnection(1, 2);
             k.AddConnection(2, 3);
             k.AddConnection(1, 3);
             k.AddConnection(3, 4);
             k.AddConnection(5, 6);
-            Console.WriteLine(k.Calculate()); // 2
+            Console.WriteLine(k.Calculate()); // 2*/
 
             //Ex4
-            /*Communication com = new Communication(6);
+            Communication com = new Communication(6);
             com.AddConnection(1, 2);
             com.AddConnection(2, 3);
             com.AddConnection(1, 3);
@@ -33,7 +33,7 @@ namespace part5
             com.AddConnection(5, 6);
             Console.WriteLine(com.Examine(1, 4)); // true
             Console.WriteLine(com.Examine(2, 5)); // false
-            Console.WriteLine(com.Examine(5, 6)); // true*/
+            Console.WriteLine(com.Examine(5, 6)); // true
 
             //Ex5
             /*Labyrinth l = new Labyrinth();
