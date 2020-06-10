ac49446791e85adac2ac089b9e9bd215e7b6e6b5
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-06-10 13:08:43 +0300

    base for ex6 done

---
+++ .vscode/Algorithms and datat structures/part6/exercise 4.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+namespace part6
+{
+    public class ShortestPath
+    {
+        public ShortestPath(int n)
+        {
+
+        }
+
+        public void AddRoad(int a, int b, int d)
+        {
+
+        }
+
+        public List<int> Create(int x, int y)
+        {
+            
+        }
+
+    }
+
+}
